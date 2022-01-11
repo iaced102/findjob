1342c16862aaaa4c5f584d6a6f209cdd8e3f4dbe
iaced102 <69673585+iaced102@users.noreply.github.com>
2022-01-11 22:46:35 +0700

    a

--- buoi1/vuhuyphuc/helloworld.py
+++ buoi1/vuhuyphuc/helloworld.py
@@ -1,12 +1,34 @@
+#số thập phân (độ chính xác 15 số sau dấu "," )
+"""
+a=5.1
+"""
+
+# số thập phân, độ chính xác xác định
+'''
+
+from decimal import *
+getcontext().prec = 20
+a = Decimal(10)/3
+ '''
+
+
+# phân số
+'''
+
 from fractions import *
 
-#a= Fraction(10,3)
-#print(a)
-#print(a+3)
+a= Fraction(10,3)
+print(a)
+print(a+3)
+'''
+
+
+#số thực
+'''
 
 b = complex(10,5)
 
 print(b + complex(3,10))
 
 print(b.real)
-print(b.imag)
+print(b.imag)'''
