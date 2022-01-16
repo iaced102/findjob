0115a47cfc8ff0f9a0f8975e1d52683e1149a7c2
thamtran2002 <89296758+thamtran2002@users.noreply.github.com>
2022-01-16 16:35:44 +0700

    scp

--- buoi3/trantham/b3.py
+++ buoi3/trantham/b3.py
@@ -31,37 +31,46 @@
 #         min=A[i]
 
 #sap xep tang dan (sx lua chon):
-a=[1, 10, 8, 20, 31, 52,17, 24,9]
-for i in range(0,len(a)-1) :
-    min = i
-    for j in range(i+1, len(a)):
-        if a[j]<a[min] :
-            min=j
-        tg = a[min]
-        a[min] = a[i]
-        a[i] = tg
-print("\nMang sap xep tang: (luachon)")
-print(a)
+# a=[1, 10, 8, 20, 31, 52,17, 24,9]
+# for i in range(0,len(a)-1) :
+#     min = i
+#     for j in range(i+1, len(a)):
+#         if a[j]<a[min] :
+#             min=j
+#         tg = a[min]
+#         a[min] = a[i]
+#         a[i] = tg
+# print("\nMang sap xep tang: (luachon)")
+# print(a)
 
-#sap xep tang dan (sx noi bot):
-a=[1, 10, 8, 20, 31, 52,17, 24,9]
-for i in range(0, len(a)-1) :
-    for j in range(0, len(a)-i-1):
-        if a[j]>a[j+1] :
-            tg = a[j]
-            a[j] = a[j+1]
-            a[j+1] = tg
-print("\nMang sap xep tang:(noibot)")
-print(a)
+# #sap xep tang dan (sx noi bot):
+# a=[1, 10, 8, 20, 31, 52,17, 24,9]
+# for i in range(0, len(a)-1) :
+#     for j in range(0, len(a)-i-1):
+#         if a[j]>a[j+1] :
+#             tg = a[j]
+#             a[j] = a[j+1]
+#             a[j+1] = tg
+# print("\nMang sap xep tang:(noibot)")
+# print(a)
 
-#sap xep tang dan:(sx chen)
-a=[1, 10, 8, 20, 31, 52,17, 24,9]
-for i in range(1, len(a)) :
-    tam = a[i]
-    j = i-1
-    while j>=0 & a[j]>tam :
-        a[j+1] = a[j]
-        j=j-1
-    a[j+1] = tam
-print("\nMang sap xep tang: (chen)")
-print(a)
+# #sap xep tang dan:(sx chen)
+# a=[1, 10, 8, 20, 31, 52,17, 24,9]
+# for i in range(1, len(a)) :
+#     tam = a[i]
+#     j = i-1
+#     while j>=0 and a[j]>tam :
+#         a[j+1] = a[j]
+#         j=j-1
+#     a[j+1] = tam
+# print("\nMang sap xep tang: (chen)")
+# print(a)
+
+#kiem tra so chinh phuong
+a = int(input("Nhap vao 1 so nguyen: "))
+for i in range(1, int(a/2)) :
+    if i**2 == a:
+       print(str(a) + " la so chinh phuong")
+    else :
+        print(str(a) + " khong la so chinh phuong")
+
