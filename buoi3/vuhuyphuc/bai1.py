dc4c2da0d0663c87280149bad42cc2c7acd73605
iaced102 <69673585+iaced102@users.noreply.github.com>
2022-01-16 15:21:08 +0700

    a

--- buoi3/vuhuyphuc/bai1.py
+++ buoi3/vuhuyphuc/bai1.py
@@ -12,7 +12,9 @@ A=[1, 20, 19, -36, 54, 17, 89, 108, -82, 30]
 min = A[0]
 
 for i in A:
+    print('i= ',i, ' min =', min)
     if i < min:
         min = i
 
 print(min)
+
---
+++ buoi3/vuhuyphuc/bai2.py
@@ -0,0 +1,18 @@
+'''
+A=[1, 20, 19, -36, 54, 17, 89, 108, -82, 30]
+sắp xếp mảng A theo thứ tự tăng dần
+
+- tìm giá trị nhỏ nhất trong khoảng từ A[0: len(A)]
+đổi vị trí giữa A[0] và giá trị nhỏ nhất
+- tìm giá trị nhỏ nhất trong khoảng từ A[1: len(A)]
+đổi vị trí giữa A[1] và giá trị nhỏ nhất trong khoảng
+.......
+
+lặp theo vị trí chứ không lặp theo giá trị
+'''
+
+
+A=[1, 20, 19, -36, 54, 17, 89, 108, -82, 30]
+
+for i in range(len(A)-1):
+    print(A[i])
