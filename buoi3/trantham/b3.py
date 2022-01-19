72ea07cb3552fea3d6f42848107cf246f08be97b
thamtran2002 <89296758+thamtran2002@users.noreply.github.com>
2022-01-19 14:55:16 +0700

    b4

--- buoi3/trantham/b3.py
+++ buoi3/trantham/b3.py
@@ -1,36 +1,55 @@
 #Them phan tu vao mang
 
-# h= []
-# h.append(10)
-# print(h)
-# for i in range(100) :
-#     if i%2==0:
-#         h.append(i)
-# print(h)
+'''
+h= []
+h.append(10)
+print(h)
+for i in range(100) :
+    if i%2==0:
+        h.append(i)
+print(h)
+
+'''
 
 # a = [i*i for i in range(10) if i%2==0]
 # print(a)
 
 #Mang 2 chieu
 
-# a=[ [1,2, [9,10]] , [3,4], [5,6], [7,8] ]
-# print(a)
+'''
+a=[ [1,2, [9,10]] , [3,4], [5,6], [7,8] ]
+print(a)
 
-# for i in a:
-#     print(i)
+for i in a:
+    print(i)
+
+'''
+
+
 
 #lay list con
-# A=[1, 10, 8, 20, 31, 52,17, 24,9]
-# A[0:3]
+'''
+A=[1, 10, 8, 20, 31, 52,17, 24,9]
+A[0:3]
+
+'''
 
 #Tim min:
-# A=[1, 10, 8, 20, 31, 52,17, 24,9]
-# min =A[0]
-# for i in range(1,len(A)):
-#     if A[i]<min:
-#         min=A[i]
+'''
+A=[1, 10, 8, 20, 31, 52,17, 24,9]
+min =A[0]
+for i in range(1,len(A)):
+    if A[i]<min:
+        min=A[i]
+'''
+
 
 #sap xep tang dan (sx lua chon):
+
+from operator import le
+from re import L
+from turtle import left, right
+
 
 a=[1, 10, 8, 20, 31, 52,17, 24,9]
 for i in range(0,len(a)-1) :
@@ -45,6 +64,7 @@ print("\nMang sap xep tang: (luachon)")
 print(a)
 
 #sap xep tang dan (sx noi bot):
+
 a=[1, 10, 8, 20, 31, 52,17, 24,9]
 for i in range(0, len(a)-1) :
     for j in range(0, len(a)-i-1):
@@ -56,6 +76,7 @@ print("\nMang sap xep tang:(noibot)")
 print(a)
 
 #sap xep tang dan:(sx chen)
+
 a=[1, 10, 8, 20, 31, 52,17, 24,9]
 for i in range(1, len(a)) :
     tam = a[i]
@@ -67,7 +88,39 @@ for i in range(1, len(a)) :
 print("\nMang sap xep tang: (chen)")
 print(a)
 
+#sap xep tang dan (sx phan doan):
+# a = []
+# left
+# right
+# n = int(input("Nhap so phan tu cua mang: "))
+# for i in range(n) :
+#     a.append(input("Nhap so thu a[%d] = " % (i+1)))
+# print("Mang vua nhap la: ", a)
+    
+# if left < right :
+#     k = (left + right)/2
+#     t=a[k]
+#     i = left
+#     j = right
+#     while i<=j :
+#         while (a[i]<t): 
+#             i=i+1
+#         while (a[j]>t): 
+#             j=j-1
+#         if i<=j:
+#             tg = a[i]
+#             a[i] = a[j]
+#             a[j] = tg
+#             i = i+1
+#             j = j-1
+
+
+
+
+
+
 #kiem tra so chinh phuong
+
 print("\n\nKiem tra so chinh phuong")
 a = int(input("\nNhap vao 1 so nguyen: "))
 check = False
@@ -81,3 +134,17 @@ if check == True :
     print(str(a) + " la so chinh phuong")
 else :
     print(str(a) + " khong la so chinh phuong")
+
+#Nhap-xuat mang 1 chieu
+'''
+a = []
+n = int(input("Nhap so phan tu cua mang: "))
+for i in range(n) :
+    a.append(input("Nhap so thu a[%d] = " % (i+1)))
+print("Mang vua nhap la: ", a)
+
+m = int(input(">>m = "))
+Myarray = list(range(m))
+print("Mang vua nhap: ", Myarray)
+
+'''
---
+++ buoi4/trantham/b4.py
@@ -0,0 +1,7 @@
+a=[1, 10, 8, 20, 31, 52,17, 24,9]
+
+for i in a:
+    if i % 2 ==0 :
+        print("Phan tu dau tien chia het cho 2 la: ", i)
+        break
+    
