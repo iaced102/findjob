e1d54405a6651601bf04e522b129c2685502bb26
mhaisiba <97507185+mhaisiba@users.noreply.github.com>
2022-01-19 15:43:22 +0700

    vd1

--- buoi 2/thanhcao.py
+++
@@ -1,5 +0,0 @@
-a = int(input("nhap gia tri a:"))
-print(sprt(a))
-print(sprt(b))
-print(sqrt(c))
-
--- buoi3/nguyenduyvuong/bai1.py
+++ buoi3/nguyenduyvuong/bai1.py
@@ -11,7 +11,18 @@ tìm giá trị nhỏ nhất của mảng
 A=[1, 20, 19, -36, 54, 17, 89, 108, -82, 30]
 '''
 A=[1, 20, 19, -36, 54, 17, 89, 108, -82, 30]
-print("so nho nhat la;",min(A))
 
+for i in range(len(A)):
+    min = A[i]
+    index_min = i
+    print(min)
+    for x in range(i,len(A)):
+        if A[x] < min:
+            min = A[x]
+            index_min = x
+            print(i, min)
+    A[i], A[index_min] = A[index_min], A[i]
+    print(A)
+print(A)  
 
 
