0c1459450c195acca747dc258b00623e6d4b8894
iaced102 <69673585+iaced102@users.noreply.github.com>
2022-01-22 15:41:28 +0700

    buoi5

--- buoi5/vuhuyphuc/buoi5.py
+++ buoi5/vuhuyphuc/buoi5.py
@@ -1,13 +1,13 @@
-def function(a):
+'''def function(a):
     return a+5
     
 
 
 a = function(20)+4
+'''
 
 
-
-
+'''
 def test(a):
     for i in range(a):
         return i
@@ -20,5 +20,16 @@ def test2(a):
     return [a,b,c,d]
 
 print(test2(10))
+'''
 
 
+'''
+- khai báo 1 hàm nhận 2 giá trị
+giá trị đầu tiên là list, giá trị thứ 2 là một số tự nhiên nhỏ hơn độ dài của list
+
+result = []
+
+lặp qua list
+for i in list:
+
+'''
